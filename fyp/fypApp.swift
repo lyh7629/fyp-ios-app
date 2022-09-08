977c030bcf75f2788076fa533158ea259cb4f7db
lyh7629 <tom19981122@gmail.com>
2022-09-09 02:45:19 +0800

    Initial Commit

---
+++ fyp/fypApp.swift
@@ -0,0 +1,17 @@
+//
+//  fypApp.swift
+//  fyp
+//
+//  Created by Yat Hin Leung on 9/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct fypApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
